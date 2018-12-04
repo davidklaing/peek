e6db5eed3e2d9f57daab18d74e8e4902df504d29
David Laing <david.laing@alumni.ubc.ca>
2018-12-03 22:01:01 -0800

    add initial file for peek()

---
+++ R/peek.R
@@ -0,0 +1,18 @@
+#' Take a peek at an object and then return it.
+#'
+#' @param .data An R object that you want to see. Usually a matrix-like object,
+#'   such as a dataframe, matrix, or list.
+#' @param view Whether to invoke a spreadsheet-style data viewer on `.data`.
+#' @param print Whether to print `.data`.
+#'
+#' @return `.data`, after having viewed or printed it.
+#' @export
+peek <- function(.data, view = TRUE, print = FALSE) {
+  if (view) {
+    View(.data)
+  }
+  if (print) {
+    print(.data)
+  }
+  return(.data)
+}
