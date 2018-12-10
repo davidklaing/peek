11a1324c7ef2b7ec907798c1f4634ab9cb8d0195
David Laing <davidkendalllaing@gmail.com>
2018-12-10 09:53:01 -0800

    remove examples because they're breaking the build

--- R/peek.R
+++ R/peek.R
@@ -3,16 +3,6 @@
 #' @param .data Any R object.
 #'
 #' @return \code{.data}, after having viewed it.
-#' @examples
-#' library(dplyr)
-#' mtcars %>%
-#'   filter(carb != 8) %>%
-#'   select(carb, mpg, cyl, hp, drat) %>%
-#'   group_by(carb) %>%
-#'   summarise(min_mpg = min(mpg)) %>%
-#'   pview() %>% # View the current state, then pass it to the next command.
-#'   mutate(min_mpg_100 = min_mpg*100)
-#'
 #' @export
 pview <- function(.data) {
   RStudioView <- as.environment("package:utils")$View
@@ -25,16 +15,6 @@ pview <- function(.data) {
 #' @param .data Any R object.
 #'
 #' @return \code{.data}, after having printed it.
-#' @examples
-#' library(dplyr)
-#' mtcars %>%
-#'   filter(carb != 8) %>%
-#'   select(carb, mpg, cyl, hp, drat) %>%
-#'   group_by(carb) %>%
-#'   summarise(min_mpg = min(mpg)) %>%
-#'   pprint() %>% # Print the current state, then pass it to the next command.
-#'   mutate(min_mpg_100 = min_mpg*100)
-#'
 #' @export
 pprint <- function(.data) {
   print(.data)
